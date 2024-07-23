c4bc9d355528eebe1580cd69495777be433348a3
Andrey <andreycesar02@gmail.com>
2024-07-22 21:32:54 -0300

    Scroll Suave

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function initTabNav() {
   const tabMenu = document.querySelectorAll('.js-tabmenu li');
   const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-
+     
   if (tabMenu.length && tabContent.length) {
     tabContent[0].classList.add(ativo);
 
@@ -46,3 +46,32 @@ function initAccordion() {
 }
 
 initAccordion();
+
+
+function initScrollSuave(){
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  
+  function scrollToSection(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  
+    /* Forma Alternativa */
+    /* const topo = section.offsetTop
+    window.scrollTo({
+      top: topo,
+      behavior: "smooth",
+    }); */
+  }
+  
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  })
+}  
+
+initScrollSuave();
